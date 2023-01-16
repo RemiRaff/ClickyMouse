c219b7e46ee68be7292926eef44d1e84ec313b20
RemiRaff <remi_raffaghello@hotmail.com>
2023-01-16 14:18:48 +0100

    Restart button fully implemented

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
 
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI highScoreText;
     public TextMeshProUGUI gameOverText;
+    public Button restartButton;
 
     public bool isGameActive;
 
     private int score;
+    private int highScore = 0;
     private float spawnRate = 1.0f;
 
     // Start is called before the first frame update
@@ -22,6 +27,7 @@ public class GameManager : MonoBehaviour
         isGameActive = true;
         // set score to zero and update
         score = 0;
+        // highScore = 0;
         UpdateScore(0);
 
         // wait and spawn target
@@ -48,11 +54,24 @@ public class GameManager : MonoBehaviour
     {
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
+        if (highScore <= score)
+        {
+            highScore = score;
+            highScoreText.text = "High Score: " + highScore;
+        }
     }
 
     public void GameOver()
     {
         gameOverText.gameObject.SetActive(true);
         isGameActive = false;
+        restartButton.gameObject.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        int save = highScore;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        highScore = save;
     }
 }
